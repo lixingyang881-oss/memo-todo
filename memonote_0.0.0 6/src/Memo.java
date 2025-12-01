07e5cff4ef587dfc8a1dc966e5d303ceba5a7c79
lixingyang881-oss <lixingyang881@gmail.com>
2025-12-01 14:25:02 +0100

    Add files via upload

---
+++ memonote_0.0.0 6/src/Memo.java
@@ -0,0 +1,9 @@
+public class Memo {
+    public String id;
+    public String title;
+    public String desc;
+    public String due;
+    public boolean important;
+    public String[] tags;
+    public String created;
+}
---
+++ memonote_0.0.0 6/src/MemoStore.java
@@ -0,0 +1,56 @@
+import java.nio.file.*;
+import java.io.IOException;
+import java.util.*;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class MemoStore {
+    private static final String DB_FILE = "memos.json";
+    private final Gson gson = new Gson();
+
+    public synchronized List<Memo> loadAll() {
+        try {
+            if (!Files.exists(Paths.get(DB_FILE))) {
+                return new ArrayList<>();
+            }
+            String json = Files.readString(Paths.get(DB_FILE));
+            if (json.isBlank()) return new ArrayList<>();
+            return gson.fromJson(json, new TypeToken<List<Memo>>(){}.getType());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    public synchronized void saveAll(List<Memo> memos) {
+        try {
+            String json = gson.toJson(memos);
+            Files.writeString(Paths.get(DB_FILE), json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void add(Memo m) {
+        List<Memo> all = loadAll();
+        all.add(m);
+        saveAll(all);
+    }
+
+    public synchronized void update(String id, Memo updated) {
+        List<Memo> all = loadAll();
+        for (int i = 0; i < all.size(); i++) {
+            if (all.get(i).id.equals(id)) {
+                all.set(i, updated);
+                break;
+            }
+        }
+        saveAll(all);
+    }
+
+    public synchronized void delete(String id) {
+        List<Memo> all = loadAll();
+        all.removeIf(m -> m.id.equals(id));
+        saveAll(all);
+    }
+}
